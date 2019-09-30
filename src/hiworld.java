930aa9895b1ebd0328127b098245dda044ebf32e
jq7888 <2058878376@qq.com>
2019-09-30 11:08:09 +0800

    git第一次提交

---
+++ src/hiworld.java
@@ -0,0 +1,13 @@
+/**
+ * ClassName:hiworld
+ * Package:PACKAGE_NAME
+ * Description:
+ *
+ * @Date:2019/9/30 10:19
+ * @Author:jiaqi@163.com
+ */
+public class hiworld {
+    public static void main(String[] args) {
+        System.out.println("hiworld ! ! !");
+    }
+}
